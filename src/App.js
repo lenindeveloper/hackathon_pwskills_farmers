54b9355810a4a8b4ca5ec48ca45902fbe29bf9c8
Lenin <lenincom2017@gmail.com>
2023-01-29 13:50:01 +0530

    route fixed

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
 
         <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
           <Routes>
-            <Route path="/" element={<MainContainer />} />
+            <Route path="/*" element={<MainContainer />} />
             <Route path="/createItem" element={<CreateContainer />} />
           </Routes>
         </main>
